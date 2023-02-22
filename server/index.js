a0c690c654ac1370168e1c466d0d06357ce2d618
Cerqueira <contatocerq@gmail.com>
2023-02-22 19:40:46 -0300

    Backend done

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 mongoose.set("strictQuery", true);
 
 //CONFIGURATIONS
@@ -60,5 +63,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`🚀 Server is running at http://localhost:${PORT}`));
+
+    //ADD DATA ONE TIME
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
